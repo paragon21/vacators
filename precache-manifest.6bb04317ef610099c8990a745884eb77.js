a76ce4451e5711a1e543bb9985f79f3365bac2dd
Konstantine <vdovin.kr@gmail.com>
2019-11-28 22:57:48 +0300

    Fix URL

---
+++ precache-manifest.6bb04317ef610099c8990a745884eb77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d32043d6c8b5d98b87cf60825a1038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f025dc7b0c47c9d3f64",
+    "url": "/static/css/main.2b797d46.chunk.css"
+  },
+  {
+    "revision": "8190386ec35563a3c811",
+    "url": "/static/js/2.8d8aea9c.chunk.js"
+  },
+  {
+    "revision": "0f025dc7b0c47c9d3f64",
+    "url": "/static/js/main.6b6d36bf.chunk.js"
+  },
+  {
+    "revision": "a10a114ed91113fa390d",
+    "url": "/static/js/runtime-main.0967760d.js"
+  },
+  {
+    "revision": "0537856ace9bff484162ba9cc1c2073a",
+    "url": "/static/media/loader.0537856a.gif"
+  }
+]);
